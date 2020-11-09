ed00b5cfa621400b0e0de1631a83adb2ac687a96
José Critóvão <cristovao.quiombo87@gmail.com>
2020-11-09 12:59:36 +0100

    Inserção dotemplate dashboard

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppTaxi.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Proprietario()
+        {
+            return View();
+        }
+        public IActionResult Passageiro()
+        {
+            return View();
+
+        }
+    }
+}
